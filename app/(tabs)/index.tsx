465ee74fef1dd2fa454182a1bb4925db7b47126b
John Barnes <ohenesetwumasi@gmail.com>
2023-09-10 19:26:41 +0000

    feat: initial commit

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,31 +1,50 @@
-import { StyleSheet } from 'react-native';
-
-import EditScreenInfo from '../../components/EditScreenInfo';
-import { Text, View } from '../../components/Themed';
-
-export default function TabOneScreen() {
+import { View, Text, Pressable } from "react-native";
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { TextInput } from "react-native-gesture-handler";
+import Feather from "@expo/vector-icons/Feather";
+import { hymns } from "../../data/hymns";
+const index = () => {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
-    </View>
-  );
-}
+    <SafeAreaView className="px-6">
+      <Text className="font-bold tracking-wide capitalize text-lg">
+        Hymn of the day
+      </Text>
+      <View>
+        <View className="flex-row items-center">
+          <Text className="text-sm">Hymn 1</Text>
+          <View className="h-1 w-1 mx-2  bg-black rounded-full"></View>
+          <Text className="text-sm">Great is thy faithfulness</Text>
+        </View>
+        <Pressable>
+          <Text className="text-xs text-gray-600">Open Now</Text>
+        </Pressable>
+      </View>
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
-});
+      <View className="flex-row items-center my-4 bg-white p-2 py-3 shadow rounded">
+        <TextInput
+          className="flex-1"
+          placeholder="Enter number, name or sentence"
+        />
+        <Feather color={"gray"} name="search" size={16} />
+      </View>
+
+      {/* Recently Viewed */}
+      <View>
+        <View className="flex-row justify-between items-center">
+          <Text className="text-lg font-bold">Recently Opened</Text>
+          <Text className="text-sm">See all</Text>
+        </View>
+        <View>
+          {hymns.map((hymn) => (
+            <View>
+              <Text>Hymn {hymn.number}</Text>
+            </View>
+          ))}
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default index;
---
+++ data/hymns.ts
@@ -0,0 +1,46 @@
+export const hymns = [
+  {
+    title: "Upon the First Day of the Week",
+    number: "1",
+    content: `
+        1. Upon the first day of the week,
+        Our Savior came forth from the grave,
+        He died, was buried, then arose,
+        To reign, to triumph, and to save!
+        CHORUS
+        Though others may forsake the Lord,
+        Yet, we are here Thy truth to seek;
+        Thy holy presence comforts us,
+        Upon the first day of the week.
+      `,
+  },
+  {
+    title: "Blessed Assurance",
+    number: "2",
+    content: `
+        1. Blessed assurance, Jesus is mine!
+        Oh, what a foretaste of glory divine!
+        Heir of salvation, purchase of God,
+        Born of His Spirit, washed in His blood.
+        CHORUS
+        This is my story, this is my song,
+        Praising my Savior all the day long;
+        This is my story, this is my song,
+        Praising my Savior all the day long.
+      `,
+  },
+  {
+    title: "Amazing Grace",
+    number: "3",
+    content: `
+        1. Amazing grace, how sweet the sound,
+        That saved a wretch like me!
+        I once was lost, but now am found,
+        Was blind, but now I see.
+        2. 'Twas grace that taught my heart to fear,
+        And grace my fears relieved;
+        How precious did that grace appear
+        The hour I first believed.
+      `,
+  },
+];
